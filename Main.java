b2b96935f9629b31b7c67f5ef4e98f72f86f94ea
majid <majid.mans@gmail.com>
2021-11-10 09:08:15 -0500

    cv

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@
 //echo '
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World!, comment ca va ?");
     }
 }
 //'  >/tmp/ajc/Main.java
